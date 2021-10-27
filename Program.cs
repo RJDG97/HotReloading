15ef0a3ee394c65176afd8eefa4b15fe7b1cf20e
Renzo Garcia <renzo_garcia@live.com>
2021-10-27 14:38:11 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input;
+
+namespace HotReloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    CompileDLL();
+                    Console.WriteLine("Compile DLL");
+                }
+                if (key.Key == ConsoleKey.Spacebar)
+                    Print();
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+            };
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Check");
+        }
+    }
+}
---
+++ test/check.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace ConsoleApp1
+{
+    class Application
+    {
+        static MethodInfo prntmethod;
+        static MethodInfo compilemethod;
+        static Object obj;
+
+        static void GetDLL()
+        {
+            string file = @"C:\Users\Renzo\source\repos\HotReloading\HotReloading.dll";
+            Assembly assembly = Assembly.LoadFile(file);
+
+            Type type = assembly.GetType("HotReloading.Program");
+
+            obj = Activator.CreateInstance(type);
+
+            prntmethod = type.GetMethod("Print");
+            compilemethod = type.GetMethod("CompileDLL");
+        }
+        public static void CompileDLL()
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters(new string[] { "System.dll" }, "test");
+            // Set compilation output to dll (true = exe, false = dll)
+            parameters.GenerateExecutable = false;
+            parameters.OutputAssembly = "HotReloading2.dll";
+            // Save as dll
+            parameters.GenerateInMemory = true;
+            // Generate DLL
+            var result = provider.CompileAssemblyFromFile(parameters, new string[] { @"C:\Users\Renzo\source\repos\HotReloading\Program.cs", @"C:\Users\Renzo\source\repos\HotReloading\test\check.cs" });
+
+            if (result.Errors.Count != 0)
+            {
+                foreach (CompilerError error in result.Errors)
+                    Console.WriteLine(error.ErrorText);
+            }
+        }
+        static void Main(string[] args)
+        {
+            GetDLL();
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    GetDLL();
+                    Console.WriteLine("Get DLL");
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    compilemethod.Invoke(obj, new object[] { });
+                    Console.WriteLine("Compile DLL");
+                }
+                if (key.Key == ConsoleKey.Spacebar)
+                    prntmethod.Invoke(obj, new object[] { });
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+            };
+        }
+    }
+}
